7246a845e658b77a0c5235898e80246b42029bc2
Zweena Ariva <ariva02zweena@gmail.com>
2025-05-01 15:21:11 +0700

    OKXPRO 2025

--- script.js
+++ script.js
@@ -5,29 +5,42 @@ $(document).ready(function() {
         $('#counter1').countTo({
             from: 0,
             to: 870,
-            speed: 3000,
+            speed: 5000,
             refreshInterval: 50
         });
         
         $('#counter2').countTo({
             from: 0,
             to: 10000,
-            speed: 3000,
+            speed: 5000,
             refreshInterval: 50
         });
         
         $('#counter3').countTo({
             from: 0,
             to: 20,
-            speed: 3000,
+            speed: 5000,
             refreshInterval: 50
         });
         
         $('#counter4').countTo({
             from: 0,
             to: 94830,
-            speed: 3000,
+            speed: 5000,
             refreshInterval: 50
         });
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const contactForm = document.getElementById('contactForm');
+  const toastElement = document.getElementById('liveToast');
+  
+  contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
+    
+    contactForm.reset();
+  });
 });
